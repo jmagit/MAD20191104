12c8734a578db7d944c8bcd63f9ab585567c6ff4
Javier <jmartin@grupoloyal.com>
2019-11-04 10:55:40 +0100

    Agregar archivos de proyecto.

---
+++ curso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(curso.Startup))]
+namespace curso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
